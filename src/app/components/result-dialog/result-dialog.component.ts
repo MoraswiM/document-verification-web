2851d25bf6383fda671fcc2973dd16a94b2b07ca
Moraswi <moraswim@tihsa.co.za>
2024-06-13 12:03:24 +0200

    new dialogue components

---
+++ src/app/components/result-dialog/result-dialog.component.ts
@@ -0,0 +1,11 @@
+import { Component, Inject } from '@angular/core';
+import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+
+@Component({
+  selector: 'app-result-dialog',
+  templateUrl: './result-dialog.component.html',
+  styleUrl: './result-dialog.component.css'
+})
+export class ResultDialogComponent {
+  constructor(@Inject(MAT_DIALOG_DATA) public data: { verified: boolean }) {}
+}
